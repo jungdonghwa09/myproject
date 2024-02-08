05d7aa9b365b030fd6cece6b65e2522e07474295
jungdonghwa09 <fgdgsg09@naver.com>
2024-02-08 10:59:45 +0900

    1번째

---
+++ src/main/java/com/donghwa/DonghwaApplication.java
@@ -0,0 +1,13 @@
+package com.donghwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonghwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonghwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donghwa/DonghwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donghwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonghwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
